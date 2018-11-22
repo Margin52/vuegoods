2c803fcf8d6f36402345361b24f04282104e8ef7
anjing <anjing@leadeon.cn>
2018-11-22 09:15:15 +0800

    路由传参，事件获取id

--- goods/src/components/goodsdec/deccontent.vue
+++ goods/src/components/goodsdec/deccontent.vue
@@ -2,12 +2,12 @@
   <div class="deccontent">
     <div class="decpeople">
       <span>收货人</span>
-      <input type="text" value="杨豪">
+      <input type="text" v-model="name">
       <img src="../../assets/images/inspeople.png" alt="">
     </div>
     <div class="dectel">
       <span>手机号码</span>
-      <input type="text" value="15202926079">
+      <input type="text" v-model="phone">
     </div>
     <div class="decadd">
       <span>所在地区</span>
@@ -16,12 +16,18 @@
     </div>
     <div class="decdel">
       <span>详细地址</span>
-      <input type="text" value="中兴产业园">
+      <input type="text" v-model="delarea">
     </div>
     <div class="decbut">
-      <img src="../../assets/images/insbut.png" alt="">
+      <img v-show="isShow" @click="changeimg" src="../../assets/images/insbut.png" alt="">
+      <img v-show="!isShow" @click="changeimg" src="../../assets/images/decbut.png" alt="">
       <span>设为默认</span>
     </div>
+    <div class="decfooter">
+    <router-link to="/">
+      <p @click="setmsg">保存</p>
+    </router-link>
+  </div>
   </div>
 </template>
 
@@ -29,6 +35,87 @@
   export default {
     data () {
       return {
+        name:'',
+        phone:'',
+        delarea:'',
+        id:'',
+        isShow:true,
+        isDefault:''
+      }
+    },
+    props:{
+      getId:String
+    },
+    mounted(){
+      this.getID()
+    },
+    methods:{
+      /**
+       * 获取id
+       */
+      getID:function(){
+        this.id = this.getId
+        // console.log("我是getId",this.id)
+        var data = JSON.parse(localStorage.getItem("goodsdata"))
+        for(var item = 0,len = data.length; item < len; item++){
+          // console.log(data[item]);
+          if(data[item].recAddrId == this.id){
+            var deldata = data[item]
+            // console.log('这是获取到的数据',deldata)
+            this.name = deldata.recName
+            this.phone = deldata.recPhoneNo
+            this.delarea = deldata.address
+          }
+        }
+      },
+      /**
+       * 默认按钮
+       */
+      changeimg:function(){
+        this.isShow = !this.isShow
+        /**
+         * 给按钮添加状态
+         * isDefault在保存用户信息的时候会用到
+         */
+        if(this.isShow){
+        /**
+         * 未设置默认
+         */
+          this.isDefault = "0"
+        }else{
+          /**
+           * 已设置默认
+           */
+          this.isDefault = "1"
+        }
+        console.log(this.isDefault)
+      },
+      /**
+       * 修改之后保存联系人信息
+       */
+      setmsg:function(){
+        var user = this.name
+        var tel = this.phone
+        var deladd = this.delarea
+        var datas = JSON.parse(localStorage.getItem("goodsdata"))
+        console.log(datas)
+        if(datas){
+          if(name && phone && delarea){
+            if(!this.isDefault || this.isDefault === "1"){
+              for(var index = 0,en = datas.length; index<en; index++){
+                data[index].checkIsUsed = "0"
+              }
+              datas.push({
+                'recName':user,
+                'recPhoneNo':tel,
+                'address':deladd,
+                'checkIsUsed':this.isDefault
+              })
+              localStorage.setItem("goodsdata",JSON.stringify(datas))
+              console.log(datas)
+            }
+          }
+        }
       }
     }
   }
@@ -80,5 +167,25 @@ input{
   width:0.42rem;
   height:0.42rem;
   margin-right:0.11rem;
+}
+.decfooter{
+  width:100%;
+  height:1.26rem;
+  background:#fff;
+  position:fixed;
+  bottom:0px;
+}
+.decfooter p{
+  width:5.88rem;
+  height:0.83rem;
+  font-size:0.27rem;
+  color:#fff;
+  background:#0085cf;
+  border-radius:0.07rem;
+  margin-left:0.3rem;
+  margin-top:0.2rem;
+  line-height:0.83rem;
+  text-align: center;
+
 }
 </style>
